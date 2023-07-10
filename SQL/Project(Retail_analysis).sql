ee492be82fa18d2ac88b1a1e12c5c138d3922162
Praveen Sharma <129173969+PraveenSharma95@users.noreply.github.com>
2023-07-10 22:41:54 +0530

    Add files via upload

---
+++ SQL/Project(Retail_analysis).sql
@@ -0,0 +1,33 @@
+create database my_project;
+use my_project
+create table login_logs(
+login_log_id int,
+user_id int,
+login_time datetime
+);
+drop login_logs;
+
+select count(*) from login_logs;
+
+-----No of user (2022) -----
+select user_id, count(*) as Numbers_of_login, login_time from  login_logs
+
+group by user_id, login_time
+order by login_time ;
+
+
+
+
+-------daily conversion rate-----
+
+select fk_buyer_id, count(order_id) as Numbers_of_orders from sales_orders
+where year(creation_time)=2021
+group by fk_buyer_id,creation
+order by Numbers_of_orders desc;
+
+
+
+
+
+---DAILY LOGINS-----
+
